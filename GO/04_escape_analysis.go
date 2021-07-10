a1048c7f48178e9157057b76276c47e03613a4df
ampacheco <ampacheco@users.noreply.github.com>
2021-07-09 21:22:55 -0400

    Create 04_escape_analysis.go

---
+++ GO/04_escape_analysis.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type user struct {
+	name  string
+	email string
+}
+
+func stayOnStack() user {
+	u := user{
+		name:  "Bill",
+		email: "bill@email.com",
+	}
+	return u
+}
+
+func main() {
+	u := stayOnStack()
+	fmt.Printf("User: %+v", stayOnStack())
+}
